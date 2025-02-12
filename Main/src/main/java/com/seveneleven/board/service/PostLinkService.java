5c535cbf2eea8effaa3ec78111240a767eba15de
DaeHyun Kim <95341286+kdh10806@users.noreply.github.com>
2025-02-12 10:34:46 +0900

    Chore: transactional 제거

--- Main/src/main/java/com/seveneleven/board/service/PostLinkService.java
+++ Main/src/main/java/com/seveneleven/board/service/PostLinkService.java
@@ -7,15 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface PostLinkService {
-    @Transactional
+
     void uploadPostLinks(List<LinkInput> linkInputs, Long postId, Long uploaderId);
 
-    @Transactional(readOnly = true)
     List<LinkResponse> getPostLinks(Long postId);
 
-    @Transactional
     void deletePostLink(Long postId, Long linkId, Long deleterId);
 
-    @Transactional
     void deleteAllPostLinks(Long postId);
 }
